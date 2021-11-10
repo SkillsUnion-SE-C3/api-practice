d9bf0fa2a9c9ca0444db5e3f56d4b59c31e9a812
Andrew Ralph-Gledhill <marleyspark@hotmail.com>
2021-11-10 21:06:56 +0000

    Add Weather

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-const url = "https://api.openweathermap.org/data/2.5/forecast/daily?q=tartu&units=metric&cnt=7&appid=55219e014878a0eda0e90952b079527d"
 
-fetch(url).then((x) => x.json()).then((x) => console.log(x))
+let weatherArry = [];
+
+//const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=aa47c4ee5bfd152d895d28621b2b1c2f"
+
+//fetch(url).then((response) => response.json()).then((data) => console.log(data))
+
+function cityName() {
+    const name = document.querySelector('#city-input').value;
+    document.querySelector("#city-name").innerHTML = name;
+    getWeather(name);
+}
+
+
+
+const getWeather = async (name) => {
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=aa47c4ee5bfd152d895d28621b2b1c2f`
+    //const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=aa47c4ee5bfd152d895d28621b2b1c2f`)
+    fetch(url).then((response) => response.json()).then((data) => weatherArry = data)
+
+    renderWeather()
+    renderWeekly()
+}
+
+//console.log(getWeather("Berlin"))
+
+const renderWeather = () => {
+
+    //description
+    const description = weatherArry.weather[0].description;
+    document.querySelector("#description").innerHTML = description;
+
+    //temp
+    const temperature = weatherArry.main.temp;
+    document.querySelector("#temperature").innerHTML = temperature;
+
+    //wind-speed
+    const windSpeed = weatherArry.wind.speed;
+    document.querySelector("#wind-speed").innerHTML = windSpeed;
+}
+
+const renderWeekly = () => {
+
+    const weeklyContainer = document.querySelector('#one-week-forecast')
+    // weeklyContainer.innerHTML = ""
+    weatherArry.forEach((weatherItem) => {
+        const weather1 = document.createElement('p')
+        weather1.textContent = weatherItem.weather[0].description
+        weeklyContainer.append(weather1)
+
+
+
+
+
+    })
+}
+
