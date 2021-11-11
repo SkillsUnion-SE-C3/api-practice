e97fc8a6ecf33c4d000c1d9069c261d9f05219ba
Andrew Ralph-Gledhill <marleyspark@hotmail.com>
2021-11-11 11:53:33 +0000

    completed 7 day

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 let weatherArry = [];
+let weeklyArry = [];
 
 //const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=aa47c4ee5bfd152d895d28621b2b1c2f"
 
@@ -9,21 +10,19 @@ function cityName() {
     const name = document.querySelector('#city-input').value;
     document.querySelector("#city-name").innerHTML = name;
     getWeather(name);
+    getWeatherWeekly(name);
 }
 
 
 
 const getWeather = async (name) => {
-
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=aa47c4ee5bfd152d895d28621b2b1c2f`
-    //const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=aa47c4ee5bfd152d895d28621b2b1c2f`)
     fetch(url).then((response) => response.json()).then((data) => weatherArry = data)
-
     renderWeather()
-    renderWeekly()
+
 }
 
-//console.log(getWeather("Berlin"))
+
 
 const renderWeather = () => {
 
@@ -38,21 +37,44 @@ const renderWeather = () => {
     //wind-speed
     const windSpeed = weatherArry.wind.speed;
     document.querySelector("#wind-speed").innerHTML = windSpeed;
+
+
 }
 
-const renderWeekly = () => {
 
-    const weeklyContainer = document.querySelector('#one-week-forecast')
-    // weeklyContainer.innerHTML = ""
-    weatherArry.forEach((weatherItem) => {
-        const weather1 = document.createElement('p')
-        weather1.textContent = weatherItem.weather[0].description
-        weeklyContainer.append(weather1)
+const getWeatherWeekly = async (name) => {
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${name}&units=metric&cnt=7&appid=55219e014878a0eda0e90952b079527d`
+    fetch(url).then((response) => response.json()).then((data) => {
+        data.list.forEach((item) => {
+            const weeklyContainer = document.createElement("div");
+            document.querySelector("#one-week-forecast").appendChild(weeklyContainer)
 
+            //week description header
+            const headerDescription = document.createElement("h5");
+            headerDescription.textContent = "Description";
+            //week description
+            const weather1 = document.createElement("p");
+            weather1.textContent = item.weather[0].description;
 
+            //header for temp
+            const headerTemp = document.createElement("h5");
+            headerTemp.textContent = "Day Temperature";
 
+            //week temp
+            const weather2 = document.createElement("p")
+            weather2.textContent = item.temp.day
 
+            //header for wind Speed
+            const headerWindSpeed = document.createElement("h5");
+            headerWindSpeed.textContent = "Wind Speed";
 
+            //week wind speed
+            const weather3 = document.createElement("p");
+            weather3.textContent = item.speed;
+
+            weeklyContainer.append(headerDescription, weather1, headerTemp, weather2, headerWindSpeed, weather3);
+        })
     })
 }
+
 
